61cea4d6bb43ab726173691af20d3e698e512c55
baybal <nikulinpi@gmail.com>
2021-05-12 15:30:20 +0600

    testcase

---
+++ src/Component.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-for="[key,value] of values">
+        
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+      values: {
+          type: Map,
+          required: true
+      }
+  }
+}
+</script>
--- src/Test.vue
+++ src/Test.vue
@@ -6,6 +6,7 @@
 
 <script>
 import * as _ from 'lodash'
+import Component from 'Component.vue'
 
 export default {
   props: ['foo', 'bar']
